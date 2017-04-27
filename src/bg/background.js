fc36d49c6c043b737b0958ddb3922af7aa95c66f
Christian Fei <crifei93@gmail.com>
2017-04-27 14:29:59 +0200

    poc forever-alone chrome extension

---
+++ src/bg/background.js
@@ -0,0 +1,8 @@
+/* globals chrome */
+
+// example of using a message handler from the inject scripts
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+  chrome.pageAction.show(sender.tab.id)
+  console.log('background.js')
+  sendResponse()
+})
